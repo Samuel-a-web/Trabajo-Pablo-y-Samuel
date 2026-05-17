8922f6a2d3e61708e133b530e6154768677952ff
Samuel-a-web <gransamu1855@gmail.com>
2026-05-17 20:13:58 +0200

    comprobacion2

--- src/app/pages/menu/menu.ts
+++ src/app/pages/menu/menu.ts
@@ -1,14 +1,14 @@
 import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { MenuService } from '../../services/menu.service';
+import { CartService } from '../../services/cart.service';
 import { MenuItemCardComponent } from '../../components/menu-item-card/menu-item-card';
 
 @Component({
   selector: 'app-menu-page',
-  standalone: true,
-  imports: [CommonModule, MenuItemCardComponent],
+  imports: [MenuItemCardComponent],
   templateUrl: './menu.html',
 })
 export class MenuPageComponent {
-  public menuService = inject(MenuService);
+  readonly menuService = inject(MenuService);
+  readonly cartService = inject(CartService);
 }
